00238bbdb681e06534ece3823215488af0a41b76
Krzysztof Komisarczyk <19d.krzysztof.komisarczyk@ezse.pl>
2022-10-10 15:14:22 +0200

    dodany x i y

--- Programowanie strukturalne i obiektowe/FirstObjectProject/FirstObjectProject.cpp
+++ Programowanie strukturalne i obiektowe/FirstObjectProject/FirstObjectProject.cpp
@@ -25,6 +25,18 @@ public:
             this->y = y;
     }
 
+    Point()
+    {
+        x = 0;
+        y = 0;
+    }
+
+   Point(int x, int y)
+    {
+        this->x = x;
+        this->y = y;
+    }
+
 protected:
 
 private:
@@ -33,11 +45,20 @@ private:
 
 int main()
 {
-    Point point;
+    int x;
+    std::cout << "Podaj x: \n";
+    std::cin >> x;
+
+    int y;
+    std::cout << "Podaj x: \n";
+    std::cin >> y;
+
+    Point point2;
+    Point point(x, y);
    //point.x = 10;
-    //point.y = 15;
-    point.SetX(10);
-    point.SetY(15);
+   //point.y = 15; 
+   //point.SetX(10);
+   //point.SetY(15);
     double distance = point.calculateDistance(point);
-    std::cout << "Dystans od punktu (0:0) = " << distance << "\n";
+    std::cout << "Dystans od punktu (0;0) = " << distance << "\n";
 }
