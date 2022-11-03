7ef10c393818b6e6daaa4da4d57c179aca445e8a
Krzysztof Komisarczyk <19d.krzysztof.komisarczyk@ezse.pl>
2022-11-03 11:37:23 +0100

    początki C#

---
+++ Programowanie strukturalne i obiektowe/FIRST Console app/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FIRST_Console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int number = 58;
+
+            Console.Write("Wartość zmiennej to:");
+            Console.WriteLine(number);
+
+            Console.WriteLine("Wartość zmiennej to: " + number + " I koniec lekcji");
+            Console.WriteLine($"Wartość zmiennej to: {number}, i koniec lekcji");
+
+            Console.WriteLine("Prędkość to km\\h");
+            Console.WriteLine(@"Prędkość to km\h");
+            Console.WriteLine("C:\\Users\\Uczen\\Desktop\\AK");
+            Console.WriteLine("C:/Users/Uczen/Desktop/AK");
+            Console.WriteLine("C:/Users/Uczen/Desktop/AK");
+            Console.WriteLine(@"C:/Users/Uczen/Desktop/AK");
+
+            if (number == 5)
+            {
+                Console.WriteLine("Równe");
+            }
+            else
+            {
+                Console.WriteLine("Nie równa się");
+
+                int number2 = number + 6;
+            }
+        }
+    }
+}
