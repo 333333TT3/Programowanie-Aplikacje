9b257f35c120f9bf7e5157f171fcd4b12cecd688
Krzysztof Komisarczyk <19d.krzysztof.komisarczyk@ezse.pl>
2022-11-07 09:54:41 +0100

    c#

---
+++ Programowanie strukturalne i obiektowe/FIRST Console app/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIRST_Console_app
+{
+    class Person
+    {
+        private string name;
+        private string surname;
+        private int age;
+
+        public Person()
+        {
+            name = "Imie";
+            surname = "Nazwisko";
+            age = 21;
+        }
+
+            public Person(string name, string surname, int age)
+            {
+                this.name = name;
+                this.surname = surname;
+                this.age = age;
+
+            }
+    }
+}
