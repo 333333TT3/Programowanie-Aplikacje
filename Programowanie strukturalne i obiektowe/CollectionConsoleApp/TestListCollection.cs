be565a54b111109c105563cc7edea9091a911e5e
Zabciol <104020296+Zabciol@users.noreply.github.com>
2022-11-28 15:14:06 +0100

    dodane metody

--- Programowanie strukturalne i obiektowe/CollectionConsoleApp/TestListCollection.cs
+++ Programowanie strukturalne i obiektowe/CollectionConsoleApp/TestListCollection.cs
@@ -17,12 +17,46 @@ namespace CollectionConsoleApp
             {
                 listOfInts.Add(random.Next(1, 11));
             }
-            Console.WriteLine("Lista int'ow");
+            Console.WriteLine("Lista int'ow:");
             foreach (int element in listOfInts)
             {
                 Console.Write(element + ", ");
             }
             Console.WriteLine();
+
+            List<double> listOfFDoubles = new List<double>();
+            for (int i = 0; i < 5; i++)
+            {
+                double number = random.NextDouble() * 10 + 1;
+                number = Math.Round(number, 4);
+
+                listOfFDoubles.Add(number);
+
+            }
+            Console.WriteLine("Lista doubl'ow:");
+            foreach (double element in listOfFDoubles)
+            {
+                Console.Write(element + ", ");
+            }
+            Console.WriteLine();
+
+            int max = MaxFromInts(listOfInts);
+            Console.WriteLine("Max w liscie int'ow: " + max);
+
+            int maxDouble = MaxFromInts(listOfDoubles);
+            Console.WriteLine("Max w liscie doubl'ow: " + maxDouble);
+
+        }
+
+        private int MaxFromInts(List<int> list)
+        {
+            int max = list[0];
+            foreach (int item in list)
+            {
+                if (item > max)
+                    max = item;
+            }
+            return max;
         }
     }
 }
