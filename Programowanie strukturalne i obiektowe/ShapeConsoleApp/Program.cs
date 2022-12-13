3046106ae2561a0e8741c2acde4b93078de422fe
Zabciol <104020296+Zabciol@users.noreply.github.com>
2022-12-13 09:55:22 +0100

    .

---
+++ Programowanie strukturalne i obiektowe/ShapeConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using ShapeConsoleApp.Shapes;
+using System;
+
+
+namespace ShapeConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle firstRectangle = new Rectangle();
+            firstRectangle.ShowInfo();
+
+            Rectangle secondRectangle = new Rectangle(54, 99);
+            secondRectangle.ShowInfo();
+
+            Square firstSquare = new Square();
+            firstSquare.ShowInfo();
+        }
+    }
+}
---
+++ Programowanie strukturalne i obiektowe/ShapeConsoleApp/Shapes/Rectangle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ShapeConsoleApp.Shapes
+{
+    class Rectangle
+    {
+        protected double sideA;
+        protected double sideB;
+
+        public Rectangle()
+        {
+            sideA = 21;
+            sideB = 37;
+        }
+
+        public Rectangle(double sideA, double sideB)
+        {
+            this.sideA = Math.Abs(sideA);
+            this.sideB = Math.Abs(sideB);
+        }
+
+        public double Area()
+        {
+            return sideA * sideB;
+        }
+
+        public double Perimeter()
+        {
+            return 2 * sideA + 2 * sideB;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Informacja o prostokącie");
+            Console.WriteLine("Bok a = " + sideA);
+            Console.WriteLine("Bok b = " + sideB);
+            Console.WriteLine("Pole " + Area());
+            Console.WriteLine("Obwód " + Perimeter());
+        }
+    }
+}
