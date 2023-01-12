bc63aa70183352d10775f7559207aa20339fd3c2
Zabciol <104020296+Zabciol@users.noreply.github.com>
2023-01-12 11:39:43 +0100

    nowe ćwiczenia

--- Programowanie strukturalne i obiektowe/LotOfTasks/Program.cs
+++ Programowanie strukturalne i obiektowe/LotOfTasks/Program.cs
@@ -21,8 +21,18 @@ namespace LotOfTasks
             //exercise.Exercise3_IF();
             //exercise.Exercise4_IF();
             //exercise.Exercise5_IF();
-            exercise.Exercise6_IF();
-            //exercise.Exercise1_petle();
+            //exercise.Exercise6_IF();
+            //exercise.Exercise1_FOR();
+            //exercise.Exercise2_FOR();
+            //exercise.Exercise3_FOR();
+            //exercise.Exercise4_FOR();
+            //exercise.Exercise5_FOR();
+            //exercise.Exercise6_FOR();
+            exercise.Exercise7_FOR();
+            //exercise.Exercise8_FOR();
+            //exercise.Exercise9_FOR();
+
+
 
             /*
             Console.WriteLine("Podaj liczbę:");
--- Programowanie strukturalne i obiektowe/LotOfTasks/exercise.cs
+++ Programowanie strukturalne i obiektowe/LotOfTasks/exercise.cs
@@ -225,22 +225,22 @@ namespace LotOfTasks
             {
                 Console.WriteLine("Data jest niepoprawna.");
             }
-                if (month > 12)
+            if (month > 12)
             {
                 Console.WriteLine("Data jest niepoprawna.");
             }
 
-           else if (month == 2 && day == 28)
-           {
+            else if (month == 2 && day == 28)
+            {
                 Console.WriteLine("Miesiac jest poprawny.");
-           }
+            }
 
-           else if (month == 2 && day == 29)
-           {
+            else if (month == 2 && day == 29)
+            {
                 Console.WriteLine("Miesiac jest poprawny.");
-           }
+            }
 
-            if (month == 4 && day <= 30 )
+            if (month == 4 && day <= 30)
             {
                 Console.WriteLine("Data jest poprawna.");
             }
@@ -265,11 +265,11 @@ namespace LotOfTasks
             Console.WriteLine("Podaj aktualna temperature:");
             string check_temperature = Console.ReadLine();
             int temperature = int.Parse(check_temperature);
-            if (temperature >= 20 )
+            if (temperature >= 20)
             {
                 Console.WriteLine("Jest cieplo");
             }
-            else if(temperature >= 11 && temperature <= 19)
+            else if (temperature >= 11 && temperature <= 19)
             {
                 Console.WriteLine("Jest nawet spoko, mozna wyjsc");
             }
@@ -283,12 +283,154 @@ namespace LotOfTasks
             }
         }
 
-        public void Exercise1_petle()
+        public void Exercise1_FOR()
         {
-            Console.WriteLine("Podaj liczbę:");
-            string check_first = Console.ReadLine();
-            int first = int.Parse(check_first);
+            for (int i = 1; i < 11; i++)
+            {
+                Console.Write(i + ", ");
+            }
+        }
 
+        public void Exercise2_FOR()
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                Console.Write(i + 101 + ", ");
+            }
+        }
+
+        public void Exercise3_FOR()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine((i * i) + "\n");
+            }
+        }
+        public void Exercise4_FOR()
+        {
+            Console.WriteLine("Podaj liczbe: ");
+            string check_silnia = Console.ReadLine();
+            int silnia = int.Parse(check_silnia);
+
+            for (int i = silnia; i > 1; i--)
+            {
+                silnia *= i;
+            }
+
+            Console.WriteLine(silnia);
+        }
+
+        public void Exercise5_FOR()
+        {
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    Console.WriteLine(i + "x" + j + "=" + (i * j));
+                }
+            }
+        }
+        public void Exercise6_FOR()
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+         public void Exercise7_FOR()
+         {
+            int equation = 1;
+            for (int i = 1; i <= 10; i++)
+             {
+                equation *= 2;
+             }
+            Console.WriteLine(equation);
+         }
+
+        /*
+         public void Exercise8_FOR()
+        {
+            for (int f = 0; f <= 20; f--)
+            {
+                for (int n = 1; n <= 20; n++)
+                {
+                   f(n) = f(n - 2) + f(n - 1);
+                }
+            }
+        }
+       */
+        public void Exercise9_FOR()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                int equation = 1;
+                for (int j = 0; j < i; j++)
+                {
+                    equation *= 2;
+                }
+                Console.WriteLine(equation);
+            }
+            // Albo krótszy i szybszy program, który domnaża kolejne liczby: 
+            double a = 0.5;
+            for (int i = 0; i <= 10; i++)
+            {
+                a *= 2;
+                Console.WriteLine("2^" + i + "=" + a);
+                
+                //Console.WriteLine(a);
+                //a *= 2;
+            }
         }
     }
 }
+
+
+/*
+ Podstawy programowania:
+1. Program obliczający średnią arytmetyczną dwóch liczb.
+2. Program obliczający pole prostokąta.
+3. Program obliczający objętość stożka.
+4. Program obliczający pole koła.
+5. Program obliczający wartość wyrażenia a^2 + b^2
+6. Program obliczający pole trójkąta o podstawie b i wysokości h
+7. Program obliczający objętość kuli o promieniu r
+8. Program obliczający pole trapezu o podstawach a i b oraz wysokości h
+9. Program obliczający średnią ważoną trzech liczb z podanymi odpowiednimi wagami w1, w2 i w3.
+Instrukcja If:
+1. Program sprawdzający czy podana liczba jest parzysta czy nieparzysta
+2. Program sprawdzający czy podana liczba jest dodatnia, ujemna czy równa zero
+3. Program sprawdzający czy podany rok jest rokiem przestępnym
+4. Program wyświetlający odpowiedni komunikat w zależności od podanej oceny (np. "bardzo dobry" dla oceny 5, "dobry" dla oceny 4 itd.)
+5. Program sprawdzający czy podane hasło jest poprawne (np. jeśli hasło jest "abc123", program powinien wyświetlić "hasło poprawne", jeśli jest inne, powinien wyświetlić "hasło niepoprawne").
+6.Program sprawdzający czy podana data jest poprawna (np. sprawdzając, czy dzień jest z zakresu od 1 do 31, miesiąc od 1 do 12 itd.)
+7. Program wyświetlający odpowiedni komunikat w zależności od podanej temperatury (np. "ciepło" dla temperatury powyżej 20 stopni Celsjusza, "chłodno" dla temperatury poniżej 10 stopni Celsjusza itd.)
+Pętle:
+1. Program sprawdzający czy podana liczba jest liczbą pierwszą (czyli taką, która dzieli się tylko przez 1 i przez siebie samą)
+2. Program sprawdzający czy podany ciąg znaków jest palindromem (czyli takim, który czytany od tyłu jest taki sam, jak czytany od przodu, np. "kajak")
+3. Program sprawdzający czy podane dwa słowa są anagramami (czyli takimi, które zawierają te same litery, ale w innym układzie, np. "klasa" i "salka")
+4. Program sprawdzający czy podana liczba jest liczbą doskonałą (czyli taką, której suma dzielników (z wyłączeniem samej siebie) jest równa danej liczbie, np. 6 jest liczbą doskonałą, ponieważ 1 + 2 + 3 = 6).
+Pętla for:
+1. Program wyświetlający na ekranie kolejne liczby naturalne od 1 do 10
+2. Program obliczający sumę liczb od 1 do 100
+3. Program wyświetlający na ekranie kwadraty liczb od 1 do 10 (np. 1, 4, 9, 16 itd.)
+4. Program wyświetlający na ekranie silnie z liczb od 1 do 10 (np. 1!, 2!, 3!, 4! itd.)
+5. Program wyświetlający na ekranie tabliczkę mnożenia od 1 do 9 (np. 1x1=1, 1x2=2, 1x3=3 itd.).
+6. Program wyświetlający na ekranie wszystkie liczby podzielne przez 3 z zakresu od 1 do 100
+7. Program obliczający sumę kwadratów liczb od 1 do 10
+8. Program wyświetlający na ekranie ciąg Fibonacciego do 20 elementu (ciąg Fibonacciego to ciąg gdzie każdy element jest sumą dwóch poprzednich, np. 0, 1, 1, 2, 3, 5, 8, 13 itd.)
+9. Program wyświetlający na ekranie kolejne potęgi 2 od 2^0 do 2^10 (np. 2^0, 2^1, 2^2 itd.)
+10. Program wyświetlający na ekranie kolejne elementy ciągu a_n = a_{n-1} + a_{n-2}, gdzie a_0=1, a_1=1 (ciąg ten zaczyna się od 1, 1, 2, 3, 5, 8, 13 itd.).
+Pętla do-while:
+1. Program pytający użytkownika o hasło i wyświetlający komunikat "hasło poprawne" lub "hasło niepoprawne" dopóki użytkownik nie poda poprawnego hasła (np. "abc123").
+2. Program wyświetlający na ekranie kwadraty liczb od 1 do 10 (np. 1, 4, 9, 16 itd.) dopóki suma tych kwadratów nie przekroczy 1000.
+Trudniejsze zadania:
+1. Program implementujący algorytm szyfrowania Cezara (proste szyfrowanie, w którym każdy znak w tekście jest zastępowany innym znakiem, przesuniętym o stałą liczbę pozycji w alfabecie).
+2. Program obliczający największy wspólny dzielnik (NWD) dwóch liczb
+3. Program obliczający najmniejszą wspólną wielokrotność (NWW) dwóch liczb.
+4. Program wyciągający informacje z numeru PESEL
+5. Napisz program, który pobiera od użytkownika ciąg znaków i wyświetla liczbę samogłosek i spółgłosek w tym ciągu.
+*/
